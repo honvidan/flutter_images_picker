e7d4926af663b55bc0ef198bd74d94d745711edf
Dan Hon <dan.honvi@gmail.com>
2022-01-26 10:26:54 +0700

    ! updated

--- lib/flutter_images_picker.dart
+++ lib/flutter_images_picker.dart
@@ -3,14 +3,23 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 
+/// FlutterImagesPicker is main class for get Images from Gallery and Camera.
+///
+/// This will call native library
 class FlutterImagesPicker {
+  /// A method channel calling native library
+  /// `_channel`
   static const MethodChannel _channel = MethodChannel('flutter_images_picker');
 
+  /// A platformVersion
+  /// `platformVersion`
   static Future<String?> get platformVersion async {
     final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
+  /// pickImages will pickImages, input maxImages
+  /// output is List of Files
   static Future<List<File>> pickImages({
     required int maxImages,
     enableGestures = true,
